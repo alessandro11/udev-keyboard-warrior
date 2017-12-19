589645205cb94baffeee0f4cf8cd8c140449d335
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-12-19 17:35:36 -0200

    Better programming practices.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- setkb.sh
+++ setkb.sh
@@ -1,50 +1,34 @@
 #!/usr/bin/env sh
 
-#if [ "$ACTION" == "add" ]; then
-#   # check if running X
-#   if xset q &>/dev/null; then
-#       #setxkbmap -layout br -variant abnt2 &> /dev/null
-#       setxkbmap -layout br -variant abnt2
-#   else
-#       loadkeys /usr/share/kbd/keymaps/i386/qwerty/br-abnt2.map.gz
-#   fi  
-#   #localectl set-keymap "$LAYOUT"
-#   #localectl set-x11-keymap "$LAYOUT"
-#elif [ "$ACTION" == "remove" ]; then
-#   # check if running X
-#   if xset q &>/dev/null; then
-#       setxkbmap -layout us
-#   else
-#       loadkeys /usr/share/kbd/keymaps/i386/qwerty/us.map.gz
-#   fi  
-#fi
+case "$ACTION" in
+	add)
+		# check if running X
+		if [ -n $DELAY ]; then
+			# delay little bit due to race condition in case is running from X
+			count=0
+			while [ $count -lt 2 ]; do
+				[ -f "/tmp/.X0-lock" ] && count=3
+				count=$(($count+1))
+				sleep 0.5
+			done
+		fi
 
-if [ "$ACTION" == "add" ]; then
-    # check if running X
-	if [ -n $DELAY ]; then
-		# delay little bit due to race condition in case is running from X
-		count=0
-		while [ $count -lt 2 ]; do
-			[ -f "/tmp/.X0-lock" ] && count=3
-			count=$(($count+1))
-			sleep 0.5
-		done
-	fi
+		if [ -f "/tmp/.X0-lock" ]; then
+			setxkbmap -layout br -variant abnt2 &> /dev/null
+		else
+			loadkeys /usr/share/kbd/keymaps/i386/qwerty/br-abnt2.map.gz
+		fi  
+		;;
 
-    if [ -f "/tmp/.X0-lock" ]; then
-        #setxkbmap -layout br -variant abnt2 &> /dev/null
-        setxkbmap -layout br -variant abnt2
-    else
-        loadkeys /usr/share/kbd/keymaps/i386/qwerty/br-abnt2.map.gz
-    fi  
-elif [ "$ACTION" == "remove" ]; then
+	remove)
     # check if running X
     if [ -f "/tmp/.X0-lock" ]; then
         setxkbmap -layout us
     else
         loadkeys /usr/share/kbd/keymaps/i386/qwerty/us.map.gz
     fi
-fi
+		;;
+esac
 
 exit 0
 
